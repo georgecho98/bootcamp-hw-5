3466076a435a265aa37200f50751d114e9f31f6f
Hon Chau <chaugeorge9@gmail.com>
2024-11-09 22:26:32 -0600

    final

--- index.js
+++ index.js
@@ -4,80 +4,105 @@ import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {type:"input",
-    name: "x",
-    message: "Do you want to create a README file?",
+    {
+        type:'confirm',
+        name: "y",
+        message: "Do you want to create a README file?",
     },
-    {type:"input",
+    {   type:"input",
         name: "title",
         message: "What is your project title?",
     },
-    {type:"input",
+    {   type:"input",
         name: "description",
         message: "Please provide the following infomration: description.",
     },
-    {type:"input",
-        name: "toc",
-        message: "Table of Content",
-    },
-    {type:"input",
+  ,
+    {   type:"input",
         name: "installation",
         message: "Installation instruction",
     },
 
-    {type:"input",
+    {   type:"input",
         name: "Usage",
         message: "Usage information",
     },
-    {type:"input",
+    {   type:"input",
         name: "Contribution",
         message: "Contribution Guidelines",
     },
-    {type:"input",
+    {   type:"input",
         name: "Test",
-        message:    "Test instructions",
+        message:"Test instructions",
     },
-    {type:"input",
+    {   type:"input",
         name: "License",
         message: "License",
     },  
-        {type:"input",
+    {   type:"input",
         name: "username",
         message: "GitHub username",
     },
-    {type:"input",
+    {   type:"input",
         name: "email",
         message: "Email address",
     }]
 
 
-    const answers = [];
-
-    
-
-
-
-
 // TODO: Create a function to write README file
-const data ="";
-
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName,data, function(err) {
+    fs.writeFile(fileName, data, 'utf8',function(err) {
         if (err) throw err;
         console.log('Saved!');
-   
-    }
-)
-
+      })
 }
 
 // TODO: Create a function to initialize app
-function init() {
+function init(x) {
 
+    inquirer
+        .prompt(x)
+        .then(response => {
+        console.info("Please input: ", response);
 
+    const content = 
+    `# ${response.title}
+    ## Description
+    ${response.description}
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
 
-    writeToFile('readme.md',data)
+    ## Installation
+    ${response.Installation}
+
+    ## Usage
+    ${response.Usage}
+
+    ## License
+    ${response.License}
+
+    ## Contributing
+    ${response.Contribution}
+
+    ## Tests
+    ${response.Test}
+
+    ## Questions
+    If you have any questions, feel free to reach out to me at [${response.email}](mailto:${response.email}).`;
+    writeToFile('readme.md', content);
+    })
+
+    .catch(error=> {
+        console.error("an error occur,", error);
+    } )       
+
 }
 
 // Function call to initialize app
-init();
+init(questions);
---
+++ readme.md
@@ -0,0 +1,29 @@
+# sdf
+    ## Description
+    sdff
+    
+    ## Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [License](#license)
+    - [Contributing](#contributing)
+    - [Tests](#tests)
+    - [Questions](#questions)
+
+    ## Installation
+    sdf
+
+    ## Usage
+    undefined
+
+    ## License
+    undefined
+
+    ## Contributing
+    undefined
+
+    ## Tests
+    undefined
+
+    ## Questions
+    If you have any questions, feel free to reach out to me at [fse](mailto:fse).
