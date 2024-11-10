7cdfd4e6b43f0484c09e38738c229f2d1ce8cabe
Hon Chau <chaugeorge9@gmail.com>
2024-11-09 19:30:19 -0600

    setup all file structure

--- index.js
+++ index.js
@@ -4,21 +4,57 @@ import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
 const questions = [
-    "Do you want to create a README file?",
-    "What is your project title?",
-    "Please provide the following infomration: description.",
-    "Installation instruction",
-    "Usage information", 
-    "Contribution Guidelines",
-    "Test instructions", 
-    "License", 
-    "GitHub username",
-    "Email address",
-    ];
+    {type:"input",
+    name: "x",
+    message: "Do you want to create a README file?",
+    },
+    {type:"input",
+        name: "title",
+        message: "What is your project title?",
+    },
+    {type:"input",
+        name: "description",
+        message: "Please provide the following infomration: description.",
+    },
+    {type:"input",
+        name: "toc",
+        message: "Table of Content",
+    },
+    {type:"input",
+        name: "installation",
+        message: "Installation instruction",
+    },
 
-    inquirer
-        .prompt(questions)
-        .then()
+    {type:"input",
+        name: "Usage",
+        message: "Usage information",
+    },
+    {type:"input",
+        name: "Contribution",
+        message: "Contribution Guidelines",
+    },
+    {type:"input",
+        name: "Test",
+        message:    "Test instructions",
+    },
+    {type:"input",
+        name: "License",
+        message: "License",
+    },  
+        {type:"input",
+        name: "username",
+        message: "GitHub username",
+    },
+    {type:"input",
+        name: "email",
+        message: "Email address",
+    }]
+
+
+    const answers = [];
+
+    
+
 
 
 
